c2bef4d4d7d77816a9a85d213f60458b5117d772
Ryan Kaplan <ryan_kaplan@me.com>
2017-09-29 14:10:10 -0700

    Allows user to start the car by pressing the "space bar".

--- main.js
+++ main.js
@@ -1,17 +1,30 @@
+const car = document.createElement('img')
+car.setAttribute('src', 'car.png')
+car.setAttribute('id', 'car')
+
+const road = document.createElement('div')
+road.setAttribute('id', 'road')
+
+document.body.appendChild(road)
+road.appendChild(car)
+
+document.addEventListener('keydown', onKeyDown, false)
+
+let intervalId = null
+
 class Car {
   constructor(direction, speed, location) {
     this.direction = direction
     this.speed = speed
     this.location = location
+    const [x, y] = location
+    car.style.left = x + 'px;'
+    car.style.top = y + 'px;'
   }
   static start(car) {
-    setInterval(function () {
+    intervalId = setInterval(function () {
       car.move()
-      console.log(car.location)
     }, 16)
-  }
-  static clone(car) {
-    return new this(car.direction, car.speed, car.location.slice())
   }
   turn(direction) {
     this.direction = direction
@@ -36,29 +49,43 @@ class Car {
   }
 }
 
-const car = document.createElement('img')
-car.setAttribute('src', 'car.png')
-car.setAttribute('id', 'car')
-
-document.body.appendChild(car)
-
-document.addEventListener('keydown', onKeyDown, false)
-
 function onKeyDown(event) {
   var keyCode = event.keyCode
   switch(keyCode){
     case 68:
       keyD = true
+      console.log('D')
       break
     case 83:
       keyS = true
+      console.log('S')
       break
-    case 63:
+    case 65:
       keyA = true
+      console.log('A')
       break
     case 87:
       keyW = true
+      console.log('W')
       break
+    case 32:
+      space = true
+      startCar()
   }
-  console.log(keyCode)
+}
+
+const porsche = new Car('South', 5, [0, 0])
+
+function startCar() {
+  if (intervalId === null) {
+    Car.start(porsche)
+    console.log('Car started!')
+  }
+  else {
+    console.log('Car already started.')
+  }
+}
+
+window.onload = function () {
+  alert('Start your car using the "space bar"!')
 }
